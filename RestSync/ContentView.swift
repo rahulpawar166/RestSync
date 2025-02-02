b11630daef7607412380006b2cae029e79064bf6
Rahul Pawar <rahulpawar166@gmail.com>
2025-02-02 15:49:45 -0500

    Initial Commit

---
+++ RestSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RestSync
+//
+//  Created by Rahul Pawar on 2/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RestSync/RestSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestSyncApp.swift
+//  RestSync
+//
+//  Created by Rahul Pawar on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct RestSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
